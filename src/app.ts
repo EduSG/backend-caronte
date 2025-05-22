a45bc5e73174293511e128b0efb66a6097cd00fd
EduSG <eduardo.gomes.072020@gmail.com>
2025-05-21 22:37:08 -0300

    feat: integração da rota na raiz

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ import { authenticateToken } from "./middlewares/auth/auth.middleware";
 import { errorMiddleware } from "./middlewares/error/error";
 import { NotificationRouter } from "./internal/notifications/routes/notification.router";
 import ScoreRouter from "./internal/score/routes/score.router";
+import TravelRouter from "./internal/travels/routes/travels.router";
 
 dotenv.config();
 
@@ -26,6 +27,7 @@ app.use("/api", authenticateToken, UsuarioRouter);
 app.use("/api", NotificationRouter)
 app.use("/api", CaronaRouter);
 app.use("/api", ScoreRouter);
+app.use("/api", TravelRouter);
 app.use(errorMiddleware);
 
 app.listen(3000, () => {
