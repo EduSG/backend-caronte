f6f71c1f1a63b8827558d2cc54d5d9ed4164d433
EduSG <eduardo.gomes.072020@gmail.com>
2025-05-21 22:35:20 -0300

    feat: tipagem da viagem

---
+++ src/internal/travels/type.ts
@@ -0,0 +1,12 @@
+export type IDBTravel = {
+  local_destino_passageiro: string 
+  local_partida_passageiro: string
+  valor: number 
+  id_passageiro: number 
+  data_criacao: string 
+  id_motorista: number 
+  local_destino_motorista: string 
+  local_partida_motorista: string 
+  horario_embarque_passageiro: string 
+  horario_desembarque_passageiro: string
+}
