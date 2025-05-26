5481465383d8bb010e15178f95baa3afc49933b3
EduSG <eduardo.gomes.072020@gmail.com>
2025-05-25 23:03:15 -0300

    feat: validação cpf duplicado

--- src/internal/users/service/user.service.ts
+++ src/internal/users/service/user.service.ts
@@ -3,13 +3,19 @@ import { IDBUser } from "../type";
 import { IDBUserSchema } from "../validator/user.validator";
 import { encryptPassword } from "../../../utils/crypt/crypt";
 import { ValidationError } from "../../../errors/errors";
+import { DuplicateError } from "../../../errors/errors";
 
 const userRepository = new UserRepository();
 
 export class UserService {
   async create(data: IDBUser) {
     const isValid = IDBUserSchema.parse(data);
+
+    const isDuplicated = await this.getByCPF(data.cpf) 
+
     if(!isValid) throw new ValidationError("Os dados não estão corretos") 
+    if(isDuplicated) throw new DuplicateError("O CPF já se encontra cadastrado")
+    
     const encryptedPassword = await encryptPassword(data.senha)
     
     const infosToSend = {
